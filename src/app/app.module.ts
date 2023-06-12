bae4ce972a9f41af0c99166413cb454402cb2360
Vijitha . P <120317975+Vijitha7-dot@users.noreply.github.com>
2023-06-12 09:52:19 +0530

    TodoList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavbarComponent,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers: [NgbModalConfig, NgbModal]
+})
+export class HomeComponent implements OnInit {
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+
+  }
+  ngOnInit(): void {
+     const localData = localStorage.getItem('todoItems');
+     if(localData != null){
+      this.todoItemsArray = JSON.parse(localData);
+     }
+  }
+  open(content: any) {
+    this.modalService.open(content);
+  }
+  todoItem: string = '';
+  todoItemsArray: string[] =[];
+  addTask(){
+    this.todoItemsArray.push(this.todoItem);
+    this.onCloseModel();
+    localStorage.setItem('todoItems',JSON.stringify(this.todoItemsArray))
+    this.todoItem = '';
+  }
+  onCloseModel(){
+    const notNull = document.getElementById('content');
+    if(notNull != null){
+      notNull.style.display = 'none';
+    }
+  }
+  deleteTask(task: string) {
+    const index = this.todoItemsArray.indexOf(task);
+    if (index > -1) {
+      this.todoItemsArray.splice(index, 1);
+      localStorage.setItem('todoItems', JSON.stringify(this.todoItemsArray));
+    }
+  }
+}
